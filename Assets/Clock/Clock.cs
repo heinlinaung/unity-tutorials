95d609b2545e53cffbaf1e5f38f9cd9e53cbb063
Hein <hein@hein.local>
2019-01-11 17:28:58 +0630

    Git init & added first tutorial(clock)

---
+++ Assets/Clock/Clock.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Clock : MonoBehaviour
+{
+    const float degreesPerHour = 30f,
+                degreesPerMinute = 6f,
+                degreesPerSecond = 6f;
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+    public bool continuous;
+
+    void Update()
+    {
+        if (continuous)
+        {
+            UpdateContinuous();
+        }
+        else
+        {
+            UpdateDiscrete();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Select Stage
+                if (hit.transform.name == "MainLink")
+                {
+                    SceneManager.LoadScene("Main");
+                }
+            }
+        }
+    }
+
+    void UpdateDiscrete()
+    {
+        DateTime time = DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+    }
+
+    void UpdateContinuous()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
+        secondsTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerSecond, 0f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
